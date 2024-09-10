38eb1dfe671290c53bfa7f1674f3d605d65b6e64
Marco Poon <marcopoon217@gmail.com>
2024-09-09 21:03:32 -0700

    fix warnings

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -3,54 +3,58 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using ZodiacApp.Models;
 using System;
 
-namespace lab_1_zodiac.Pages;
-
-public class IndexModel : PageModel
+namespace lab_1_zodiac.Pages
 {
-    private readonly ILogger<IndexModel> _logger;
-
-    [BindProperty(SupportsGet = true)]
-    public int? Year { get; set; }
-    public string ZodiacSign { get; set; }
-    public bool DisplayError { get; set; }
-
-    public IndexModel(ILogger<IndexModel> logger)
+    public class IndexModel : PageModel
     {
-        _logger = logger;
-    }
+        private readonly ILogger<IndexModel> _logger;
 
-    public void OnGet()
-    {
-        if (Year.HasValue && Year >= 1900 && Year <= DateTime.Now.Year + 1)
+        [BindProperty(SupportsGet = true)]
+        public int? Year { get; set; }
+
+        // Make ZodiacSign nullable
+        public string? ZodiacSign { get; set; }
+
+        public bool DisplayError { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
         {
-            ZodiacSign = Utils.GetZodiac(Year.Value);
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            if (Year.HasValue && Year >= 1900 && Year <= DateTime.Now.Year + 1)
+            {
+                ZodiacSign = Utils.GetZodiac(Year.Value);
+                DisplayError = false;
+            }
+            else if (Year.HasValue)
+            {
+                DisplayError = true;
+                ZodiacSign = null;
+            }
+        }
+
+        public void OnPost()
+        {
+            if (Year.HasValue && Year >= 1900 && Year <= DateTime.Now.Year + 1)
+            {
+                ZodiacSign = Utils.GetZodiac(Year.Value);
+                DisplayError = false;
+            }
+            else
+            {
+                DisplayError = true;
+                ZodiacSign = null;
+            }
+        }
+
+        public void OnPostClear()
+        {
+            Year = null;
+            ZodiacSign = null;
             DisplayError = false;
         }
-        else if (Year.HasValue)
-        {
-            DisplayError = true;
-            ZodiacSign = null;
-        }
-    }
-
-    public void OnPost()
-    {
-        if (Year.HasValue && Year >= 1900 && Year <= DateTime.Now.Year + 1)
-        {
-            ZodiacSign = Utils.GetZodiac(Year.Value);
-            DisplayError = false;
-        }
-        else
-        {
-            DisplayError = true;
-            ZodiacSign = null;
-        }
-    }
-
-    public void OnPostClear()
-    {
-        Year = null;
-        ZodiacSign = null;
-        DisplayError = false;
     }
 }
